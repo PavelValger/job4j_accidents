7f84195d193c26547c83bc8438b3ae4636bbfb0c
Pavel Valger <pavelwalker@mail.ru>
2023-11-01 22:49:53 +0500

    0. Spring MVC [#6877]

--- src/main/java/ru/job4j/accidents/Main.java
+++ src/main/java/ru/job4j/accidents/Main.java
@@ -1,8 +1,13 @@
 package ru.job4j.accidents;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Main.class, args);
+        System.out.println("Go to http://localhost:8080/");
     }
 }
