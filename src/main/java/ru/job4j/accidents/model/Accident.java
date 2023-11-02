c37ffe6cf8ff98a0b53f8f5f160ee20f6ea305c0
Pavel Valger <pavelwalker@mail.ru>
2023-11-02 21:03:17 +0500

    2. IndexController. Таблица и вид. [#2092]

---
+++ src/main/java/ru/job4j/accidents/model/Accident.java
@@ -0,0 +1,20 @@
+package ru.job4j.accidents.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.EqualsAndHashCode.Include;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Accident {
+    @Include
+    private int id;
+    private String name;
+    private String text;
+    @Include
+    private String address;
+}
---
+++ src/main/java/ru/job4j/accidents/service/SimpleAccidentService.java
@@ -0,0 +1,24 @@
+package ru.job4j.accidents.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.job4j.accidents.model.Accident;
+import ru.job4j.accidents.repository.AccidentRepository;
+
+import java.util.Collection;
+
+@Service
+@RequiredArgsConstructor
+public class SimpleAccidentService implements AccidentService {
+    private final AccidentRepository accidentRepository;
+
+    @Override
+    public Accident save(Accident accident) {
+        return accidentRepository.save(accident);
+    }
+
+    @Override
+    public Collection<Accident> findAll() {
+        return accidentRepository.findAll();
+    }
+}
