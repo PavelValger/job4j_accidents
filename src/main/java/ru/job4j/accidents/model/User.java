41731fd962e0c978b5baf76146fdf955abeca0ae
Pavel Valger <pavelwalker@mail.ru>
2023-11-14 21:12:13 +0500

    2. Регистрация пользователя [#296069]

---
+++ src/main/java/ru/job4j/accidents/model/User.java
@@ -0,0 +1,27 @@
+package ru.job4j.accidents.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.EqualsAndHashCode.Include;
+
+import javax.persistence.*;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Include
+    private Integer id;
+    @Include
+    private String username;
+    private String password;
+    private boolean enabled;
+
+    @ManyToOne
+    @JoinColumn(name = "authority_id")
+    private Authority authority;
+}
